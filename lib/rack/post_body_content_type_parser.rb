4517c8302e67503693d111f027d2b583906307a7
Matt Todd <chiology@gmail.com>
2008-12-02 03:44:46 -0500

    Added the PostBodyContentTypeParser middleware and tests. Tests will be skipped if the JSON gem is not installed, issuing a warning.

---
+++ lib/rack/post_body_content_type_parser.rb
@@ -0,0 +1,40 @@
+begin
+  require 'json'
+rescue LoadError => e
+  require 'json/pure'
+end
+
+module Rack
+  
+  # A Rack middleware for parsing POST/PUT body data when Content-Type is
+  # not one of the standard supported types, like <tt>application/json</tt>.
+  # 
+  # TODO: Find a better name.
+  # 
+  class PostBodyContentTypeParser
+    
+    # Constants
+    # 
+    CONTENT_TYPE = 'CONTENT_TYPE'.freeze
+    POST_BODY = 'rack.input'.freeze
+    FORM_INPUT = 'rack.request.form_input'.freeze
+    FORM_HASH = 'rack.request.form_hash'.freeze
+    
+    # Supported Content-Types
+    # 
+    APPLICATION_JSON = 'application/json'.freeze
+    
+    def initialize(app)
+      @app = app
+    end
+    
+    def call(env)
+      case env[CONTENT_TYPE]
+      when APPLICATION_JSON
+        env.update(FORM_HASH => JSON.parse(env[POST_BODY].read), FORM_INPUT => env[POST_BODY])
+      end
+      @app.call(env)
+    end
+    
+  end
+end
---
+++ test/spec_rack_post_body_content_type_parser.rb
@@ -0,0 +1,31 @@
+require 'rack/mock'
+
+begin
+  require 'rack/post_body_content_type_parser'
+  
+  context "Rack::PostBodyContentTypeParser" do
+    
+    specify "should handle requests with POST body Content-Type of application/json" do
+      app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, Rack::Request.new(env).POST] }
+      env = env_for_post_with_headers('/', {'Content_Type'.upcase => 'application/json'}, {:body => "asdf", :status => "12"}.to_json)
+      body = Rack::PostBodyContentTypeParser.new(app).call(env).last
+      body['body'].should.equal "asdf"
+      body['status'].should.equal "12"
+    end
+    
+    specify "should change nothing when the POST body content type isn't application/json" do
+      app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, Rack::Request.new(env).POST] }
+      body = app.call(Rack::MockRequest.env_for("/", :input => "body=asdf&status=12")).last
+      body['body'].should.equal "asdf"
+      body['status'].should.equal "12"
+    end
+    
+  end
+  
+  def env_for_post_with_headers(path, headers, body)
+    Rack::MockRequest.env_for(path, {:method => "POST", :input => body}.merge(headers))
+  end
+rescue LoadError => e
+  # Missing dependency JSON, skipping tests.
+  STDERR.puts "skipping ostBodyContentTypeParser tests - install the JSON gem to enable"
+end
