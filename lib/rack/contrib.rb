8c303380670b6e1310f92520f9849146d9376dea
Matt Todd <chiology@gmail.com>
2008-12-02 04:08:58 -0500

    Added JSONP and PostBodyContentTypeParser to the autoload in rack/contrib.rb.

--- lib/rack/contrib.rb
+++ lib/rack/contrib.rb
@@ -5,9 +5,11 @@ module Rack
       "0.4"
     end
   end
-
-  autoload :ContentLength, "rack/content_length"
-  autoload :ETag, "rack/etag"
-  autoload :MailExceptions, "rack/mailexceptions"
-  autoload :Sendfile, "rack/sendfile"
+  
+  autoload :ContentLength,              "rack/content_length"
+  autoload :ETag,                       "rack/etag"
+  autoload :MailExceptions,             "rack/mailexceptions"
+  autoload :Sendfile,                   "rack/sendfile"
+  autoload :JSONP,                      "rack/jsonp"
+  autoload :PostBodyContentTypeParser,  "rack/post_body_content_type_parser"
 end
