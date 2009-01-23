44322288f263dda0743183529efe4e04a240e76c
Cameron Walters <cameron.walters@gmail.com>
2009-01-23 01:05:20 -0800

    Wrap callback tests in a common context

--- test/spec_rack_jsonp.rb
+++ test/spec_rack_jsonp.rb
@@ -4,20 +4,24 @@ require 'rack/contrib/jsonp'
 
 context "Rack::JSONP" do
 
-  specify "should wrap the response body in the Javascript callback when provided" do
-    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, '{"bar":"foo"}'] }
-    request = Rack::MockRequest.env_for("/", :input => "foo=bar&callback=foo")
-    body = Rack::JSONP.new(app).call(request).last
-    body.should == 'foo({"bar":"foo"})'
-  end
+  context "when a callback parameter is provided" do
+    specify "should wrap the response body in the Javascript callback" do
+      test_body = '{"bar":"foo"}'
+      callback = 'foo'
+      app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, test_body] }
+      request = Rack::MockRequest.env_for("/", :input => "foo=bar&callback=#{callback}")
+      body = Rack::JSONP.new(app).call(request).last
+      body.should == "#{callback}(#{test_body})"
+    end
 
-  specify "should modify the content length to the correct value when callback is provided" do
-    test_body = '{"bar":"foo"}'
-    callback = 'foo'
-    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, test_body] }
-    request = Rack::MockRequest.env_for("/", :input => "foo=bar&callback=#{callback}")
-    headers = Rack::JSONP.new(app).call(request)[1]
-    headers['Content-Length'].should == (test_body.length + callback.length + 2).to_s # 2 parentheses
+    specify "should modify the content length to the correct value" do
+      test_body = '{"bar":"foo"}'
+      callback = 'foo'
+      app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, test_body] }
+      request = Rack::MockRequest.env_for("/", :input => "foo=bar&callback=#{callback}")
+      headers = Rack::JSONP.new(app).call(request)[1]
+      headers['Content-Length'].should == (test_body.length + callback.length + 2).to_s # 2 parentheses
+    end
   end
 
   specify "should not change anything if no callback param is provided" do
