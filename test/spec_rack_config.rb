01533a8a88fce359d6ec07058d0ed7b13b8ed839
Jon Crosby <jon@joncrosby.me>
2009-02-01 15:19:55 -0800

    Add Rack::Config
    Rack::Config is shared configuration for cooperative middleware,
    pulled from the rack-config gem.

---
+++ test/spec_rack_config.rb
@@ -0,0 +1,22 @@
+require 'test/spec'
+require 'rack/mock'
+require 'rack/contrib/config'
+
+context "Rack::Config" do
+
+  specify "should accept a block that modifies the environment" do
+    app = Rack::Builder.new do
+      use Rack::Lint
+      use Rack::ContentLength
+      use Rack::Config do |env|
+        env['greeting'] = 'hello'
+      end
+      run lambda { |env|
+        [200, {'Content-Type' => 'text/plain'}, [env['greeting'] || '']]
+      }
+    end
+    response = Rack::MockRequest.new(app).get('/')
+    response.body.should.equal('hello')
+  end
+
+end
