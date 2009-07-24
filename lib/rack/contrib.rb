5ea5e585a43669842314aa07f1e603be70d6e288
Paul Sadauskas <psadauskas@gmail.com>
2009-07-25 05:16:12 +0800

    Add Rack::Runtime to autoloads
    Signed-off-by: Joshua Peek <josh@joshpeek.com>

--- lib/rack/contrib.rb
+++ lib/rack/contrib.rb
@@ -22,6 +22,7 @@ module Rack
   autoload :PostBodyContentTypeParser,  "rack/contrib/post_body_content_type_parser"
   autoload :ProcTitle,                  "rack/contrib/proctitle"
   autoload :Profiler,                   "rack/contrib/profiler"
+  autoload :Runtime,                    "rack/contrib/runtime"
   autoload :Sendfile,                   "rack/contrib/sendfile"
   autoload :Signals,                    "rack/contrib/signals"
   autoload :TimeZone,                   "rack/contrib/time_zone"
