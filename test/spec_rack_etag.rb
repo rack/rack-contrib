9c9f8d8f2c4bd2c94c7a19deac4e2729877bfe67
James Rosen <jrosen@mitre.org>
2010-02-05 19:03:00 -0500

    Rack::ETag now supports SHA-1 and SHA-2 in addition to MD5

--- test/spec_rack_etag.rb
+++ test/spec_rack_etag.rb
@@ -1,12 +1,38 @@
 require 'test/spec'
 require 'rack/mock'
 require 'rack/contrib/etag'
+require 'digest/md5'
+require 'digest/sha1'
+require 'digest/sha2'
 
 context "Rack::ETag" do
-  specify "sets ETag if none is set" do
-    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, "Hello, World!"] }
-    response = Rack::ETag.new(app).call({})
-    response[1]['ETag'].should.equal "\"65a8e27d8879283831b664bd8b7f0ad4\""
+
+  body   = 'Hello, World!'
+  
+  context('if no ETag is set on a String body') do
+    before(:each) do
+      @app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, body] }
+    end
+    specify 'sets ETag' do
+      response = Rack::ETag.new(@app).call({})
+      response[1]['ETag'].should.equal "\"#{Digest::MD5.hexdigest(body)}\""
+    end
+    specify 'uses SHA-1 if specified' do
+      response = Rack::ETag.new(@app, :sha1).call({})
+      response[1]['ETag'].should.equal "\"#{Digest::SHA1.hexdigest(body)}\""
+    end
+    specify 'uses SHA-256 if specified' do
+      response = Rack::ETag.new(@app, :sha256).call({})
+      response[1]['ETag'].should.equal "\"#{Digest::SHA2.hexdigest(body)}\""
+    end
+    specify 'uses SHA-384 if specified' do
+      response = Rack::ETag.new(@app, :sha384).call({})
+      response[1]['ETag'].should.equal "\"#{(Digest::SHA2.new(384) << body).to_s}\""
+    end
+    specify 'uses SHA-512 if specified' do
+      response = Rack::ETag.new(@app, :sha512).call({})
+      response[1]['ETag'].should.equal "\"#{(Digest::SHA2.new(512) << body).to_s}\""
+    end
   end
 
   specify "does not change ETag if it is already set" do
@@ -15,7 +41,7 @@ context "Rack::ETag" do
     response[1]['ETag'].should.equal "\"abc\""
   end
 
-  specify "does not set ETag if steaming body" do
+  specify "does not set ETag if streaming body" do
     app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello", "World"]] }
     response = Rack::ETag.new(app).call({})
     response[1]['ETag'].should.equal nil
