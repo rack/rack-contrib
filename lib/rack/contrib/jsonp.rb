f95cffcecb80eb5b93b8657e0e46293426c93ab9
Rune Botten <rbotten@gmail.com>
2010-05-28 14:06:23 +0200

    Fix JSONP to correctly update content-length and only if set. Change content type to application/javascript. Not wrap in callback unless JSON.

--- lib/rack/contrib/jsonp.rb
+++ lib/rack/contrib/jsonp.rb
@@ -5,24 +5,47 @@ module Rack
   # Full credit to Flinn Mueller (http://actsasflinn.com/) for this contribution.
   #
   class JSONP
-
+    include Rack::Utils
     def initialize(app)
       @app = app
     end
 
     # Proxies the request to the application, stripping out the JSON-P callback
-    # method and padding the response with the appropriate callback format.
+    # method and padding the response with the appropriate callback format if
+    # the returned body is application/json
     #
     # Changes nothing if no <tt>callback</tt> param is specified.
     #
     def call(env)
       status, headers, response = @app.call(env)
+      headers = HeaderHash.new(headers)
       request = Rack::Request.new(env)
-      if request.params.include?('callback')
+      
+      if is_json?(headers['Content-Type']) && has_callback?(request.params)
         response = pad(request.params.delete('callback'), response)
-        headers['Content-Length'] = response.length.to_s
+
+        # No longer json, its javascript!
+        headers['Content-Type'].gsub!('json', 'javascript')
+        
+        # Set new Content-Length, if it was set before we mutated the response body
+        if headers['Content-Length']
+          # Code from Rack::ContentLength
+          response = [response] if response.respond_to?(:to_str) # rack 0.4 compat
+          length = response.to_ary.inject(0) { |len, part| len + bytesize(part) }
+          headers['Content-Length'] = length.to_s
+        end
       end
       [status, headers, response]
+    end
+    
+    private
+    
+    def is_json?(header)
+      header.include?('application/json')
+    end
+    
+    def has_callback?(params)
+      params.include?('callback')
     end
 
     # Pads the response with the appropriate callback format according to the
--- test/spec_rack_jsonp.rb
+++ test/spec_rack_jsonp.rb
@@ -5,27 +5,55 @@ require 'rack/contrib/jsonp'
 context "Rack::JSONP" do
 
   context "when a callback parameter is provided" do
-    specify "should wrap the response body in the Javascript callback" do
+    specify "should wrap the response body in the Javascript callback if JSON" do
+      test_body = '{"bar":"foo"}'
+      callback = 'foo'
+      app = lambda { |env| [200, {'Content-Type' => 'application/json'}, [test_body]] }
+      request = Rack::MockRequest.env_for("/", :params => "foo=bar&callback=#{callback}")
+      body = Rack::JSONP.new(app).call(request).last
+      body.should.equal "#{callback}(#{test_body})"
+    end
+    
+    specify "should not wrap the response body in a callback if body is not JSON" do
       test_body = '{"bar":"foo"}'
       callback = 'foo'
       app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, [test_body]] }
       request = Rack::MockRequest.env_for("/", :params => "foo=bar&callback=#{callback}")
       body = Rack::JSONP.new(app).call(request).last
-      body.should.equal "#{callback}(#{test_body})"
+      body.join.should.equal '{"bar":"foo"}'
     end
-
-    specify "should modify the content length to the correct value" do
+    
+    specify "should update content length if it was set" do
       test_body = '{"bar":"foo"}'
       callback = 'foo'
-      app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, [test_body]] }
+      app = lambda { |env| [200, {'Content-Type' => 'application/json', 'Content-Length' => test_body.length}, [test_body]] }
       request = Rack::MockRequest.env_for("/", :params => "foo=bar&callback=#{callback}")
       headers = Rack::JSONP.new(app).call(request)[1]
       headers['Content-Length'].should.equal((test_body.length + callback.length + 2).to_s) # 2 parentheses
     end
+    
+    specify "should not touch content length if not set" do
+      test_body = '{"bar":"foo"}'
+      callback = 'foo'
+      app = lambda { |env| [200, {'Content-Type' => 'application/json'}, [test_body]] }
+      request = Rack::MockRequest.env_for("/", :params => "foo=bar&callback=#{callback}")
+      headers = Rack::JSONP.new(app).call(request)[1]
+      headers['Content-Length'].should.equal nil
+    end
+    
+    specify "should modify the content type to application/javascript" do
+      test_body = '{"bar":"foo"}'
+      callback = 'foo'
+      app = lambda { |env| [200, {'Content-Type' => 'application/json'}, [test_body]] }
+      request = Rack::MockRequest.env_for("/", :params => "foo=bar&callback=#{callback}")
+      headers = Rack::JSONP.new(app).call(request)[1]
+      headers['Content-Type'].should.equal('application/javascript')
+    end
+    
   end
 
   specify "should not change anything if no callback param is provided" do
-    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['{"bar":"foo"}']] }
+    app = lambda { |env| [200, {'Content-Type' => 'application/json'}, ['{"bar":"foo"}']] }
     request = Rack::MockRequest.env_for("/", :params => "foo=bar")
     body = Rack::JSONP.new(app).call(request).last
     body.join.should.equal '{"bar":"foo"}'
