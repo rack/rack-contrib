07022018ff063c96b8b92151ad741f8abed58ebb
gmarik <gmarik@gmail.com>
2010-09-19 12:16:17 -0500

    Rack::TryStatic middleware
    - try match request to a static file

---
+++ lib/rack/contrib/try_static.rb
@@ -0,0 +1,36 @@
+module Rack
+
+  # The Rack::TryStatic middleware delegates requests to Rack::Static middleware 
+  # trying to match a static file
+  # 
+  # Examples
+  #
+  # use Rack::TryStatic, 
+  #   :root => "public",  # static files root dir
+  #   :urls => %w[/],     # match all requests 
+  #   :try => ['.html', 'index.html', '/index.html'] # try these postfixes sequentially
+  #
+  #   uses same options as Rack::Static with extra :try option which is an array
+  #   of postfixes to find desired file
+
+  class TryStatic
+
+    def initialize(app, options)
+      @app = app
+      @try = ['', *options.delete(:try)]
+      @static = ::Rack::Static.new(
+        lambda { [404, {}, []] }, 
+        options)
+    end
+
+    def call(env)
+      orig_path = env['PATH_INFO']
+      found = nil
+      @try.each do |path|
+        resp = @static.call(env.merge!({'PATH_INFO' => orig_path + path}))
+        break if 404 != resp[0] && found = resp
+      end 
+      found or @app.call(env.merge!('PATH_INFO' => orig_path))
+    end
+  end
+end
