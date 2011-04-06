ecab8e1b85b69dca65a3f924932a6fad9c6a3193
Erik Eldridge <erik@twitter.com>
2011-04-06 00:53:58 -0700

    Create test for custom cookie name

--- test/spec_rack_ab.rb
+++ test/spec_rack_ab.rb
@@ -10,7 +10,7 @@ context "Rack::AB" do
     app = Rack::AB.new(app)
 
     response = Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => '')
-    ['rack_ab=a','rack_ab=b'].should.include(response.headers['Set-Cookie'])
+    response.headers['Set-Cookie'].should =~ /rack_ab=[a,b]/
   end
   
   specify "should not set a cookie if one is already defined" do
@@ -21,6 +21,16 @@ context "Rack::AB" do
 
     response = Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => 'rack_ab=a')
     response.headers['Set-Cookie'].should == nil
+  end
+  
+  specify "provides a way to set a custom cookie name" do
+    app = lambda { |env|
+      [200, {'Content-Type' => 'text/plain'}, '']
+    }
+    app = Rack::AB.new(app, 'new_cookie_name')
+
+    response = Rack::MockRequest.new(app).get('/', 'HTTP_COOKIE' => '')
+    response.headers['Set-Cookie'].should =~ /new_cookie_name=[a,b]/
   end
   
 end
