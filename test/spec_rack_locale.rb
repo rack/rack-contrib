6e1c9c5c386813e48d2daa39802cd06c95973e58
James A. Rosen <james@zendesk.com>
2012-08-18 16:58:14 -0700

    add some specs for Rack::Locale

---
+++ test/spec_rack_locale.rb
@@ -0,0 +1,45 @@
+require 'test/spec'
+require 'rack/mock'
+
+context "Rack::Locale" do
+
+  setup do
+    begin
+      require 'rack/contrib/locale'
+    rescue LoadError
+      warn "I18n required for Rack::Locale specs"
+    end
+  end
+
+  def app
+    @app ||= Rack::Builder.new do
+      use Rack::Locale
+      run lambda { |env| [ 200, {}, [ I18n.locale.to_s ] ] }
+    end
+  end
+
+  def response_with_languages(accept_languages)
+    Rack::MockRequest.new(app).get('/', { 'HTTP_ACCEPT_LANGUAGE' => accept_languages } )
+  end
+
+  specify 'should use I18n.default_locale if no languages are requested' do
+    I18n.default_locale = :zh
+    response_with_languages(nil).body.should.equal('zh')
+  end
+
+  specify 'should treat an empty qvalue as 1.0' do
+    response_with_languages('en,es;q=0.95').body.should.equal('en')
+  end
+
+  specify 'should set the Content-Language response header' do
+    headers = response_with_languages('de;q=0.7,dk;q=0.9').headers
+    headers['Content-Language'].should.equal('dk')
+  end
+
+  specify 'should reset the I18n locale after the response' do
+    I18n.locale = 'es'
+    response_with_languages('en,de;q=0.8')
+    I18n.locale.should.equal(:es)
+  end
+
+end
