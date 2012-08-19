0c1a7ac13374012643444f847e5883a39bca9149
James A. Rosen <james@zendesk.com>
2012-08-18 17:16:31 -0700

    Rack::Locale: extract helper method

--- lib/rack/contrib/locale.rb
+++ lib/rack/contrib/locale.rb
@@ -8,26 +8,31 @@ module Rack
 
     def call(env)
       old_locale = I18n.locale
-      locale = nil
-
-      # http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
-      if lang = env["HTTP_ACCEPT_LANGUAGE"]
-        lang = lang.split(",").map { |l|
-          l += ';q=1.0' unless l =~ /;q=\d+(?:\.\d+)?$/
-          l.split(';q=')
-        }.sort_by { |(locale, qvalue)|
-          qvalue.to_f
-        }.last
-        locale = lang.first.split("-").first
-      else
-        locale = I18n.default_locale
-      end
-
+      locale = accept_locale(env) || I18n.default_locale
       locale = env['rack.locale'] = I18n.locale = locale.to_s
       status, headers, body = @app.call(env)
       headers['Content-Language'] = locale
       I18n.locale = old_locale
       [status, headers, body]
+    end
+
+    private
+
+    # http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+    def accept_locale(env)
+      accept_langs = env["HTTP_ACCEPT_LANGUAGE"]
+      return if accept_langs.nil?
+
+      languages_and_qvalues = accept_langs.split(",").map { |l|
+        l += ';q=1.0' unless l =~ /;q=\d+(?:\.\d+)?$/
+        l.split(';q=')
+      }
+
+      lang = languages_and_qvalues.sort_by { |(locale, qvalue)|
+        qvalue.to_f
+      }.last
+
+      lang.first.split("-").first
     end
   end
 end
