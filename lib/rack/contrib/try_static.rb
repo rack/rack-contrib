fafccebc5dab66f6155ff72b7a9e35452124fac4
Sebastian Glazebrook <sebastian.glazebrook@rea-group.com>
2013-09-26 11:16:36 +1000

    Only looks for static files if its a get request

--- lib/rack/contrib/try_static.rb
+++ lib/rack/contrib/try_static.rb
@@ -26,9 +26,11 @@ module Rack
     def call(env)
       orig_path = env['PATH_INFO']
       found = nil
-      @try.each do |path|
-        resp = @static.call(env.merge!({'PATH_INFO' => orig_path + path}))
-        break if 404 != resp[0] && found = resp
+      if env['REQUEST_METHOD'] == 'GET'
+        @try.each do |path|
+          resp = @static.call(env.merge!({'PATH_INFO' => orig_path + path}))
+          break if 404 != resp[0] && found = resp
+        end
       end
       found or @app.call(env.merge!('PATH_INFO' => orig_path))
     end
