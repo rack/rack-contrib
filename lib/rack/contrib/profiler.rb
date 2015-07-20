fbd216e18d824fb32e553c1134f4ca652dc0dc33
Matt Palmer <mpalmer@hezmatt.org>
2015-07-20 16:20:48 +1000

    Add docs, don't use instance variables for per-request info

--- lib/rack/contrib/profiler.rb
+++ lib/rack/contrib/profiler.rb
@@ -5,6 +5,12 @@ module Rack
   # calltree profile of the request.
   #
   # Pass the :printer option to pick a different result format.
+  #
+  # You can cause every request to be run multiple times by passing the
+  # `:times` option to the `use Rack::Profiler` call.  You can also run a
+  # given request multiple times, by setting the `profiler_runs` query
+  # parameter in the request URL.
+  #
   class Profiler
     MODES = %w(process_time wall_time cpu_time
                allocations memory gc_runs gc_time)
@@ -27,7 +33,6 @@ module Rack
 
     def call(env)
       if mode = profiling?(env)
-        @times = @request.params['times'].to_i if @request.params['times']
         profile(env, mode)
       else
         @app.call(env)
@@ -37,8 +42,8 @@ module Rack
     private
       def profiling?(env)
         unless ::RubyProf.running?
-          @request = Rack::Request.new(env.clone)
-          if mode = @request.params.delete('profile')
+          request = Rack::Request.new(env.clone)
+          if mode = request.params.delete('profile')
             if ::RubyProf.const_defined?(mode.upcase)
               mode
             else
@@ -54,8 +59,10 @@ module Rack
         ::RubyProf.measure_mode = ::RubyProf.const_get(mode.upcase)
 
         GC.enable_stats if GC.respond_to?(:enable_stats)
+        request = Rack::Request.new(env.clone)
+        runs = (request.params['profiler_runs'] || @times).to_i
         result = ::RubyProf.profile do
-          @times.times { @app.call(env) }
+          runs.times { @app.call(env) }
         end
         GC.disable_stats if GC.respond_to?(:disable_stats)
 
