e3fc7ce80315587c65f6af9d1e5714aa28ad0ea6
Mig <mig@campbellhay.com>
2015-07-22 15:23:19 +0100

    Clean up the tests for LazyConditionalGet

--- test/spec_rack_lazy_conditional_get.rb
+++ test/spec_rack_lazy_conditional_get.rb
@@ -6,58 +6,59 @@ $lazy_conditional_get_cache = {}
 
 describe 'Rack::LazyConditionalGet' do
 
-  def core_app
-    lambda { |env| [200, core_app_headers, ['data']] }  
-  end
-
-  def core_app_headers
-    { 
-      'Content-Type' => 'text/plain', 
-      'Rack-Lazy-Conditional-Get' => rack_lazy_conditional_get 
-    } 
-  end
-
-  def rack_lazy_conditional_get; 'yes'; end
-
-  def app
-    Rack::LazyConditionalGet.new core_app, cache_object
-  end
-
-  def env env_headers={}
-    Rack::MockRequest.env_for path, env_headers
-  end
-
-  def path; '/'; end
-  def cache_object; $lazy_conditional_get_cache; end
-
   def global_last_modified
-    cache_object[Rack::LazyConditionalGet::KEY]
+    $lazy_conditional_get_cache[Rack::LazyConditionalGet::KEY]
   end
 
   def set_global_last_modified val
-    cache_object[Rack::LazyConditionalGet::KEY] = val
+    $lazy_conditional_get_cache[Rack::LazyConditionalGet::KEY] = val
   end
 
-  def request_with_stamp stamp=nil
-    myapp = app
-    myenv = case stamp
-            when nil
-              {}
-            when :up_to_date
-              {'HTTP_IF_MODIFIED_SINCE' => global_last_modified}
-            else
-              {'HTTP_IF_MODIFIED_SINCE' => stamp}
-            end
-    myapp.call(env(myenv))
-  end
-  def request_without_stamp
-    request_with_stamp nil
-  end
+  let(:core_app) {
+    lambda { |env| [200, response_headers, ['data']] }  
+  }
+
+  let(:response_headers) {
+    headers = { 
+      'Content-Type' => 'text/plain', 
+      'Rack-Lazy-Conditional-Get' => rack_lazy_conditional_get 
+    }
+    if response_with_last_modified
+      headers.merge!({'Last-Modified' => (Time.now-3600).httpdate})
+    end
+    headers 
+  }
+
+  let(:response_with_last_modified) { false }
+
+  let(:rack_lazy_conditional_get) { 'yes' }
+
+  let(:app) {
+    Rack::LazyConditionalGet.new core_app, $lazy_conditional_get_cache
+  }
+
+  let(:env) {
+    Rack::MockRequest.env_for '/', request_headers
+  }
+
+  let(:request_headers) {
+    headers = { 'REQUEST_METHOD' => request_method }
+    if request_with_current_date
+      headers.merge!({'HTTP_IF_MODIFIED_SINCE' => global_last_modified})
+    end
+    headers
+  }
+
+  let(:request_method) { 'GET' }
+
+  let (:request_with_current_date) { false }
+
+  before { @myapp = app }
 
   describe 'When the resource has Rack-Lazy-Conditional-Get' do
 
     it 'Should set right headers' do
-      status, headers, body = request_without_stamp
+      status, headers, body = @myapp.call(env)
       status.must_equal 200
       headers['Rack-Lazy-Conditional-Get'].must_equal 'yes'
       headers['Last-Modified'].must_equal global_last_modified
@@ -65,12 +66,10 @@ describe 'Rack::LazyConditionalGet' do
 
     describe 'When the resource already has a Last-Modified header' do
 
-      def core_app_headers
-        super.merge({'Last-Modified' => (Time.now-3600).httpdate})
-      end
+      let(:response_with_last_modified) { true }
 
       it 'Does not update Last-Modified with the global one' do
-        status, headers, body = request_without_stamp
+        status, headers, body = @myapp.call(env)
         status.must_equal 200
         headers['Rack-Lazy-Conditional-Get'].must_equal 'yes'
         headers['Last-Modified'].wont_equal global_last_modified
@@ -80,10 +79,11 @@ describe 'Rack::LazyConditionalGet' do
 
     describe 'When loading a resource for the second time' do
 
-      def core_app; lambda { |env| raise }; end
+      let(:core_app) { lambda { |env| raise } }
+      let(:request_with_current_date) { true }
 
       it 'Should not render resource the second time' do
-        status, headers, body = request_with_stamp :up_to_date
+        status, headers, body = @myapp.call(env)
         status.must_equal 304
       end
 
@@ -93,25 +93,23 @@ describe 'Rack::LazyConditionalGet' do
 
   describe 'When a request is potentially changing data' do
 
+    let(:request_method) { 'POST' }
+
     it 'Updates the global_last_modified' do
-      myapp = app
       set_global_last_modified (Time.now-3600).httpdate
       stamp = global_last_modified
-      status, headers, body = myapp.call(env({'REQUEST_METHOD' => 'POST'}))
+      status, headers, body = @myapp.call(env)
       global_last_modified.wont_equal stamp
     end
 
     describe 'When the skip header is returned' do
       
-      def core_app_headers
-        super.merge({'Rack-Lazy-Conditional-Get' => 'skip'})
-      end
+      let(:rack_lazy_conditional_get) { 'skip' }
 
       it 'Does not update the global_last_modified' do
-        myapp = app
         set_global_last_modified (Time.now-3600).httpdate
         stamp = global_last_modified
-        status, headers, body = myapp.call(env({'REQUEST_METHOD' => 'POST'}))
+        status, headers, body = @myapp.call(env)
         headers['Rack-Lazy-Conditional-Get'].must_equal 'skip'
         global_last_modified.must_equal stamp
       end
@@ -122,10 +120,10 @@ describe 'Rack::LazyConditionalGet' do
 
   describe 'When the ressource does not have Rack-Lazy-Conditional-Get' do
 
-    def rack_lazy_conditional_get; 'no'; end
+    let(:rack_lazy_conditional_get) { 'no' }
 
     it 'Should set right headers' do
-      status, headers, body = request_without_stamp
+      status, headers, body = @myapp.call(env)
       status.must_equal 200
       headers['Rack-Lazy-Conditional-Get'].must_equal 'no'
       headers['Last-Modified'].must_be :nil?
