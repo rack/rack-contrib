e4c2bb0551055655fe14b79a6a8bb5dabd11b3a1
Armin Fröhlich <mail@arminfroehlich.de>
2018-09-06 20:28:19 +0200

    add_transform_keys, review adaptions
    check if `Content-Type` equals `json`
    regular expressions constantized and frozen
    adding examples and code style

--- lib/rack/contrib/transform_keys.rb
+++ lib/rack/contrib/transform_keys.rb
@@ -1,62 +1,102 @@
 module Rack
-  # It will transform incoming params Hash keys to snake case,
-  # outgoing params Hash keys will be transformed to camel case.
-  # Useful if you work on your Front-End in camel case, and on your Back-End in snake case.
-  # Credits to the Rails developer's for the Regular Expression's.
+  #
+  # TransformKeys will transform json request param keys to snake case,
+  # response json param keys will be transformed to camel case.
+  # Useful if you work on your client-side in camel case, and on your server-side in snake case.
+  # Credits to the Rails developers for the Regular Expressions.
+  #
+  #  Example 1,
+  #  client sends application/json request with following data:
+  #    { 'newName' => { 'firstName' => 'henry', 'lastName' => 'whoever' } }
+  #
+  #  server application will receive the following object:
+  #    { 'new_name' => { 'first_name' => 'henry', 'last_name' => 'whoever' } }
+  #
+  #  Example 2,
+  #  server application renders application/json response with following data:
+  #    { 'new_name' => { 'first_name' => 'henry', 'last_name' => 'whoever' } }
+  #
+  #  client will receive the following object:
+  #    { 'newName' => { 'firstName' => 'henry', 'lastName' => 'whoever' } }
+  #
+  #  Within the Rails framework this could be easily achieved with:
+  #    params.deep_transform_keys { |key| key.to_s.underscore }
+  #    response_hash.deep_transform_keys { |key| key.to_s.camelize(:lower) }
+  #  But not every rack based web application is powered by rails and generally
+  #  this shouldn't be done by the application, that's a middleware task.
+  #
 
   class TransformKeys
     UNDERSCORE = '_'.freeze
+
+    # to_camel_case RegEx's
+    UPPERCASE_FIRST_LETTER = /^(?:(?=a)b(?=\b|[A-Z_])|\w)/.freeze
+    UNDERSCORE_FOLLOWED_BY_WORD = /(?:_|(\/))([a-z\d]*)/i.freeze
+
+    # to_snake_case RegEx's
+    MISTERY_REG_EX = /(?:(?<=([A-Za-z\d]))|\b)((?=a)b)(?=\b|[^a-z])/.freeze
+    UPPERCASE_FOLLOWED_BY_LOWERCASE_LETTER = /([A-Z\d]+)([A-Z][a-z])/.freeze
+    LOWERCASE_FOLLOWED_UPPERCASE_LETTER = /([a-z\d])([A-Z])/.freeze
 
     def initialize(app)
       @app = app
     end
 
     def call(env)
-      Rack::Request.new(env).params
-      env['rack.request.query_hash'] = _transform_keys(env['rack.request.query_hash'])
-      env['rack.request.form_vars']  = _transform_keys(env['rack.request.form_vars'])
-      status, _, body                = @app.call(env)
-      new_body                       = []
-
-      body.each do |b|
-        json_b = JSON.parse(b)
-        new_body.push(json_b[0] => json_b[1])
+      if content_type_json?(env)
+        Rack::Request.new(env).params
+        env['rack.request.query_hash'] = transform_keys(env['rack.request.query_hash'])
+        env['rack.request.form_vars']  = transform_keys(env['rack.request.form_vars'])
       end
 
-      transformed_body = _transform_keys(new_body, true)
-      Rack::Response.new(transformed_body.map(&:to_json), status)
+      status, header, body = @app.call(env)
+
+      if content_type_json?(header)
+        new_body = []
+        body.each do |b|
+          json_b = JSON.parse(b)
+          new_body.push(json_b[0] => json_b[1])
+        end
+        body = transform_keys(new_body, true).map(&:to_json)
+      end
+
+      Rack::Response.new(body, status, header)
     end
 
     private
 
-    def _to_camel_case(str0)
-      str1 = str0.to_s.gsub(/^(?:(?=a)b(?=\b|[A-Z_])|\w)/, &:downcase)
-      str2 = str1.gsub(/(?:_|(\/))([a-z\d]*)/i, &:capitalize)
-      str2.gsub(/(?:_|(\/))([a-z\d]*)/i) do
+    def content_type_json?(obj)
+      obj['Content-Type'] && obj['Content-Type'] == 'application/json'
+    end
+
+    def to_camel_case(str0)
+      str1 = str0.to_s.gsub(UPPERCASE_FIRST_LETTER, &:downcase)
+      str2 = str1.gsub(UNDERSCORE_FOLLOWED_BY_WORD, &:capitalize)
+      str2.gsub(UNDERSCORE_FOLLOWED_BY_WORD) do
         "#{Regexp.last_match(1)}#{Regexp.last_match(2).capitalize}"
       end
     end
 
-    def _to_snake_case(str0)
-      str1 = str0.to_s.gsub(/(?:(?<=([A-Za-z\d]))|\b)((?=a)b)(?=\b|[^a-z])/) do
+    def to_snake_case(str0)
+      str1 = str0.to_s.gsub(MISTERY_REG_EX) do
         "#{Regexp.last_match(1) && UNDERSCORE}#{Regexp.last_match(2).downcase}"
       end
-      str2 = str1.gsub(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2'.freeze)
-      str3 = str2.gsub(/([a-z\d])([A-Z])/, '\1_\2'.freeze)
+      str2 = str1.gsub(UPPERCASE_FOLLOWED_BY_LOWERCASE_LETTER, '\1_\2'.freeze)
+      str3 = str2.gsub(LOWERCASE_FOLLOWED_UPPERCASE_LETTER, '\1_\2'.freeze)
       str3.downcase
     end
 
-    def _transform_keys(object, camelize = false)
+    def transform_keys(object, camelize = false)
       case object
       when Hash
         object.keys.each do |key|
           value           = object.delete(key)
-          new_key         = camelize ? _to_camel_case(key) : _to_snake_case(key)
-          object[new_key] = _transform_keys(value, camelize)
+          new_key         = camelize ? to_camel_case(key) : to_snake_case(key)
+          object[new_key] = transform_keys(value, camelize)
         end
         object
       when Array
-        object.map! { |e| _transform_keys(e, camelize) }
+        object.map! { |e| transform_keys(e, camelize) }
       else
         object
       end
--- test/spec_rack_transform_keys.rb
+++ test/spec_rack_transform_keys.rb
@@ -4,66 +4,72 @@ require 'rack/contrib/transform_keys'
 require 'json'
 
 describe 'Rack::TransformKeys' do
-  def request(incoming_params, outcoming_params)
+  def request(request_params, response_params)
     transformed_keys_app = lambda do |env|
       # check if transformed keys are equal to our expectations
       # after testing subject had been called
       request = Rack::Request.new(env)
-      request.params.must_equal(outcoming_params)
-      Rack::Response.new(request.params.map(&:to_json), 200)
+      request.params.must_equal(response_params)
+      Rack::Response.new(request.params.map(&:to_json), 200, 'Content-Type' => 'application/json')
     end
 
     mock_app = lambda do |env|
       if env['QUERY_STRING'].empty?
-        copied_incoming_params = JSON.parse(incoming_params.to_json)
+        copied_request_params = JSON.parse(request_params.to_json)
 
         # set params
         request = Rack::Request.new(env)
-        incoming_params.each { |k, v| request.update_param(k, v) }
+        request_params.each { |k, v| request.update_param(k, v) }
       end
 
       # call testing subject
       status, header, body = Rack::TransformKeys.new(transformed_keys_app).call(env)
 
-      # set body to proper object
+      # set body to array of json objects
       new_body = []
       body.each do |b|
         new_body.push(JSON.parse(b))
       end
 
       # test for equality
-      new_body.must_equal([copied_incoming_params || incoming_params])
+      new_body.must_equal([copied_request_params || request_params])
 
       [status, header, new_body]
     end
     @request = Rack::MockRequest.new(mock_app)
   end
 
+  def json_request(request_params, response_params, query_path)
+    request(request_params, response_params).get(query_path, 'Content-Type' => 'application/json')
+  end
+
   specify 'simple hash for body' do
-    request({ 'newName' => 'henry whoever' }, 'new_name' => 'henry whoever').get('/')
+    json_request({ 'newName' => 'henry whoever' }, { 'new_name' => 'henry whoever' }, '/')
   end
 
   specify 'nested hash for body' do
-    request(
+    json_request(
       { 'newName' => { 'firstName' => 'henry', 'lastName' => 'whoever' } },
-      'new_name' => { 'first_name' => 'henry', 'last_name' => 'whoever' }
-    ).get('/')
+      { 'new_name' => { 'first_name' => 'henry', 'last_name' => 'whoever' } },
+      '/'
+    )
   end
 
   specify 'simple hash as query string' do
     query_string = 'newName=henry%20whoever'
-    request({ 'newName' => 'henry whoever' }, 'new_name' => 'henry whoever').get("/?#{query_string}")
+    json_request({ 'newName' => 'henry whoever' }, { 'new_name' => 'henry whoever' }, "/?#{query_string}")
   end
 
   specify 'nested hash as query string' do
     query_string = 'newName[firstName]=henry&newName[lastName]=whoever'
-    request(
+    json_request(
       { 'newName' => { 'firstName' => 'henry', 'lastName' => 'whoever' } },
-      'new_name' => { 'first_name' => 'henry', 'last_name' => 'whoever' }
-    ).get("/?#{query_string}")
+      { 'new_name' => { 'first_name' => 'henry', 'last_name' => 'whoever' } },
+      "/?#{query_string}"
+    )
   end
 
   specify 'just a simple query_string' do
-    request({ 'string' => nil }, 'string' => nil).get('/?string')
+    json_request({ 'string' => nil }, { 'string' => nil }, '/?string')
   end
 end
